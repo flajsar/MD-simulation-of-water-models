b0b84d46aa16d9e4b49027d13739bed85d4f8ed9
flajsar <141168345+flajsar@users.noreply.github.com>
2023-08-01 18:54:59 +0200

    Add files via upload

---
+++ 4-site model/potential_fnc.f90
@@ -0,0 +1,192 @@
+﻿
+    
+    module pot_4site
+    use basic_fnc
+    use omp_lib
+    implicit none
+        !vsebuje subroutines za izračun potenciala, sil in virial
+        !lj_del_pot -> zračuna lj del potenciala,sile,virial med kisiki 
+        ! ewald_sum-> zračuna elektrostatkse sile kot ewaldovo vsoto
+    
+    
+    contains
+    
+    subroutine lj_del_pot(x,delec,a,N,l,a1,a2,potencial_lj,virial)
+
+        implicit none
+        integer, intent(in)::N
+        real(8),intent(in)::x(4*N,3),delec(4*N,2)
+        real(8),intent(in)::l,a1,a2
+        real(8),intent(out)::a(3*N,3),potencial_lj,virial
+        !temp spremenljivke:
+        real(8),allocatable::f(:,:,:)
+        real(8)::x1,x2,r,r2,temppot,v1(3),v2(3),mol(3,3)
+        integer::i,j
+    
+        allocate(f(3*N,3*N,3))
+        f=0
+        
+        
+        !zračunamo LJ potencial in sledeče sile med kisiki
+        do i=1,N
+            do j=i+1,N
+                 r2=len_sq(diff12(x(j,:),x(i,:),l))
+                 r=dsqrt(r2)
+                 x1=(a2/r)**3
+                 x1=x1**2
+                 temppot=4*a1*x1*(x1-1)
+                 x2=-24*a1*x1*(2*x1-1)
+                 potencial_lj=potencial_lj+temppot
+                 f(i,j,:)=-x2/r2*diff12(x(j,:),x(i,:),l)
+                 f(j,i,:)=-f(i,j,:)
+                 virial=virial-dot_product(f(i,j,:),diff12(x(j,:),x(i,:),l))
+                 
+            end do
+        end do
+        do i=1,N
+            a(i,1)=a(i,1)+sum(f(i,:,1)) !sešteje sile da dobimo vsoto sil na atom i 
+            a(i,2)=a(i,2)+sum(f(i,:,2))
+            a(i,3)=a(i,3)+sum(f(i,:,3))
+        end do
+        
+        end subroutine
+        
+        
+        
+        
+        subroutine ewald_sum(x,delec,a,N,l,G,eps0,virial,potencial_el,k,self_int,r_om)
+
+        implicit none
+        integer, intent(in)::N,k
+        real(8),intent(in)::x(4*N,3),delec(4*N,2),eps0
+        real(8),intent(in)::l,G,self_int,r_om 
+        real(8),intent(out)::a(3*N,3),potencial_el,virial
+        !temp spremenljivke:
+        real(8),allocatable::f(:,:,:),vf(:,:)
+        real(8)::x1,x2,r,r2,pi,sqpi,fakt1,fakt2,temppot,potencial_el_t
+        integer::i,j,k1,k2,k3
+        real(8)::vec_k(3),rec_v(3,3),cntr_mass(3),d_i(4,3),force(3),mol(4,3),vs_re(4*N),vs_im(4*N)
+        complex(8)::vs1,vs2,im,y1,t,vs1t
+        
+        allocate(f(4*N,4*N,3),vf(4*N,3))
+        f=0
+        vf=0
+        pi=atan(1.)*4
+        sqpi=dsqrt(pi)
+        potencial_el_t=0
+        !zračunamo short-range del elektrostatskega potenciala za vse atome
+        !$OMP PARALLEL DO default (none) &
+        !$OMP& schedule(dynamic) &
+        !$OMP& SHARED(N,x,l,G,pi,eps0,f,sqpi,delec) &
+        !$OMP& reduction(+:potencial_el_t) &
+        !$OMP& private(r2,r,fakt1,fakt2,x1,x2,temppot,j,i)
+        do i=1,4*N
+            do j=i+1,4*N
+                r2=len_sq(diff12(x(j,:),x(i,:),l)) !kvadrat razdalje med atomoma
+                r=sqrt(r2) 
+                fakt1=nint(mod(real(j-i),real(N))/real(N) +0.5 -1.0/(N+1)) !fakt1 in fakt2 sta uporabljena zato da določita če sta atoma del iste molekule
+                fakt2=((fakt1+1.0)/2.0)**(-1)-1  
+                x1=fakt1*erfc(G*r)-fakt2*erf(G*r) !če sta del iste molekule: fakt1=0, fakt2=1 in obratno
+                x2=delec(i,2)*delec(j,2) !naboj*naboj
+                temppot=x2*x1/(r*4.0*pi*eps0)
+
+                potencial_el_t=temppot+potencial_el_t
+                
+
+                f(i,j,:)=(diff12(x(j,:),x(i,:),l))*(temppot+(4.0*pi*eps0)**(-1)*  2 * G/sqpi * x2 * exp(-G**2 * r2))/r2
+                f(j,i,:)=-f(i,j,:)
+                
+            end do
+        end do
+        !$omp end parallel do
+        
+        do i=1,4*N
+            vf(i,1)=vf(i,1)+sum(f(i,:,1)) !sešteje sile da dobimo vsoto sil na atom i 
+            vf(i,2)=vf(i,2)+sum(f(i,:,2))
+            vf(i,3)=vf(i,3)+sum(f(i,:,3))
+        end do
+        virial=virial-potencial_el_t
+        potencial_el=potencial_el_t+potencial_el
+        
+        !recipročni del potenciala
+        temppot=0
+        vs1=(0.0,0.0)
+        vs2=vs1
+        im=(0.0,1.0)
+        rec_v(1,:)=(/1.d0,0.d0,0.d0/)
+        rec_v(2,:)=(/0.d0,1.d0,0.d0/)
+        rec_v(3,:)=(/0.d0,0.d0,1.d0/)
+        rec_v(1,:)=rec_v(1,:)*2*pi/l
+        rec_v(2,:)=rec_v(2,:)*2*pi/l
+        rec_v(3,:)=rec_v(3,:)*2*pi/l
+        do k1=-k,k
+            do k2=-k,k
+                do k3=-k,k
+                    if ((k1 .eq. 0) .and. (k2 .eq. 0) .and. (k3 .eq. 0)) then
+                    else
+                        vec_k=k1*rec_v(1,:)+k2*rec_v(2,:)+k3*rec_v(3,:)
+                        vs1=(0.0,0.0)
+                        vs2=vs1
+                        fakt1=exp(-len_sq(vec_k)/(4*g**2))/len_sq(vec_k)
+                        vs_re=0
+                        vs_im=0
+                        !$OMP PARALLEL DO &
+                        !$OMP& default (shared) &
+                        !$OMP& private(vs1,i)  &
+                        !$OMP& reduction(+:vs_re,vs_im)
+                        do i=1,4*N
+                            vs1t=delec(i,2)*cdexp(im*dot_product(vec_k,x(i,:)))
+                            vs_re(i)=real(vs1t,8)
+                            vs_im(i)=dimag(vs1t)
+                        end do
+                        !$omp end parallel do
+                        vs1t=sum(vs_re)+sum(vs_im)*im
+                        vs2=DCONJG(vs1t)
+                        t=vs1t*vs2
+                        temppot=temppot+fakt1*t%re
+                        do j=1,4*N
+                            y1=cdexp(im*dot_product(vec_k,x(j,:))) * vs2
+                            force=vec_k(:)*(delec(j,2)/(eps0*l**3) * fakt1 * y1%IM)
+                            vf(j,:)=vf(j,:)+force
+                        end do
+                        end if
+                end do 
+            end do
+        end do
+
+        
+        potencial_el=temppot/(2*eps0*l**3)+potencial_el
+        virial=virial-temppot/(2*eps0*l**3)
+        
+        !prištejemo self interaction potencial
+        potencial_el=potencial_el-self_int
+        virial=virial+self_int
+        
+        !atomic to molecular virial correction
+        do i=1,N
+            mol(1,:)=x(i,:)
+            mol(2,:)=x(i+n,:)
+            mol(3,:)=x(i+2*n,:)
+            mol(4,:)=x(i+3*n,:)
+            mol(2,:)=mol(2,:)+l*int((mol(1,:)-mol(2,:))*2/l)
+            mol(3,:)=mol(3,:)+l*int((mol(1,:)-mol(3,:))*2/l)
+            mol(4,:)=mol(4,:)+l*int((mol(1,:)-mol(4,:))*2/l)
+            cntr_mass=delec(i,1)*mol(1,:)+delec(i+n,1)*mol(2,:)+delec(i+n*2,1)*mol(3,:)
+            cntr_mass=cntr_mass/(delec(i,1)+delec(i+n,1)+delec(i+n*2,1))
+            d_i(1,:)=mol(1,:)-cntr_mass
+            d_i(2,:)=mol(2,:)-cntr_mass
+            d_i(3,:)=mol(3,:)-cntr_mass
+            d_i(4,:)=mol(4,:)-cntr_mass
+            virial=virial+(dot_product(vf(i,:),d_i(1,:))+dot_product(vf(i+n,:),d_i(2,:))+dot_product(vf(i+n*2,:),d_i(3,:))+dot_product(vf(i+n*3,:),d_i(4,:)))
+            
+        end do
+        
+        call project_realf(vf,a,N,r_om,x,l) !preračuna sile iz 'dummy' atoma na realne
+        
+        
+        
+        end subroutine
+    
+    
+end module
+    
---
+++ 4-site model/termo_barostats_fnc.f90
@@ -0,0 +1,189 @@
+﻿module termo_barostat
+    use basic_fnc
+    implicit none
+    
+    
+    contains
+    
+    subroutine berendsen_termostat(v,temp0,temp,delec,N,tau,dt)
+        implicit none
+        integer,intent(in)::N
+        real(8),intent(in)::temp0,delec(4*N,2),tau,dt
+        real(8),intent(inout)::v(3*N,3),temp
+        real(8)::ksi
+        integer::i
+        
+        temp=0
+        do i=1,3*N
+            temp=temp+delec(i,1)*len_sq(v(i,:)) !zračuna temp kot vsoto m*v**2
+        end do
+        temp=temp/(6*N-3) !prejšna vsota se deli z 3*(N-1)-N_c kjer je N_c število constraints-v tem primeru so 3 na molekulo
+        ksi=sqrt(1-dt/tau *(1-temp0/temp)) !berendsenov faktor
+        v(:,:)=ksi*v(:,:)
+    end subroutine
+    
+    
+    
+    
+    
+    
+    subroutine berendsen_barostat(pressure,pressure0,ni_bar,tau_bar,l,izoterm_st,dt)
+        implicit none
+        real(8),intent(in)::pressure,pressure0,tau_bar,izoterm_st,dt
+        real(8),intent(inout)::ni_bar,l
+        ni_bar=(1-izoterm_st * dt/tau_bar *(pressure0 - pressure))**(1./3.)
+        l=l*ni_bar
+    end subroutine
+    
+     subroutine remove_comm(x,v,N,delec)
+        implicit none
+        real(8),intent(in)::x(4*n,3),delec(4*N,2)
+        integer,intent(in)::N
+        real(8),intent(inout)::v(3*n,3)
+        
+        integer::i
+        real(8)::comm(3),mass
+        comm=0
+        mass=0
+        do i=1,3*N
+            comm=comm+v(i,:)*delec(i,1)
+            mass=mass+delec(i,1)
+        end do
+        comm=comm/mass
+        do i=1,3*n
+            v(i,:)=v(i,:)-comm
+        end do
+        
+        
+     end subroutine
+     
+     
+     
+     
+     subroutine calc_th_eta(th_q,th_eta,v,dt,delec,N,temp0)
+     integer,intent(in)::N
+     real(8),intent(in)::dt,th_q,delec(4*N,2),temp0,v(3*N,3)
+     real(8),intent(inout)::th_eta
+     integer::i
+     real(8)::sum=0
+     sum=0
+     do i=1,3*N
+        sum=sum+delec(i,1)*len_sq(v(i,:))
+     end do
+     th_eta=th_eta + dt* (sum-(6*N-3)*temp0) /(4*th_q)
+     end subroutine
+     
+     subroutine nh_v_adjust(v,dt,th_eta,N)
+     real(8),intent(in)::dt,th_eta
+     integer, intent(in):: N
+     real(8),intent(inout):: v(3*N,3)
+     
+        v=v-v*dt*th_eta/2
+     end subroutine
+     
+     
+     subroutine calc_temp(v,N,delec,temp)
+     integer,intent(in)::N
+     real(8),intent(in)::delec(4*N,2),v(3*N,3)
+     real(8),intent(out)::temp
+     integer::i
+     temp=0
+     do i=1,3*N
+         temp=temp+delec(i,1)*len_sq(v(i,:)) 
+    end do
+    temp=temp/(6*N-3)
+     end subroutine
+     
+     
+     subroutine mtk_calc_th_eta(th_eta,dt,th_q,mtk_w,N,v,delec,temp0,mtk_nib)
+     implicit none
+     integer,intent(in):: N
+     real(8),intent(in)::dt,th_q,mtk_w,v(3*N,3),temp0,delec(4*n,2),mtk_nib
+     real(8),intent(inout)::th_eta
+     real(8)::vs,nf
+     integer::i
+     nf=6*n-3
+     vs=0
+     do i=1,3*n
+         vs=vs+delec(i,1)*len_sq(v(i,:))
+     end do
+     
+     th_eta=th_eta+ dt/(4*th_q) * (  vs - nf*temp0 + mtk_w * mtk_nib**2-temp0)
+     
+     end subroutine
+     
+     
+     subroutine calc_mtk_nib(th_eta,dt,mtk_w,N,v,delec,virial,l,mtk_nib,pressure0,x,a)
+     implicit none
+     real(8),intent(inout)::mtk_nib
+     integer,intent(in):: N
+     real(8),intent(in):: virial,v(3*n,3),l,delec(4*n,2),x(4*n,2),a(3*n,3),dt,mtk_w,pressure0,th_eta
+     real(8)::p,t   
+     
+     call calc_pressure(virial,v,l,N,p,delec,x,a)
+    call calc_temp(v,N,delec,t)
+    
+     mtk_nib=mtk_nib + dt/4 * ( 3/mtk_w * l**3 * (p-pressure0) + 3/mtk_w * t - th_eta* mtk_nib)
+     end subroutine
+     
+     
+     subroutine mtk_v_adjust(v,th_eta,N,dt,mtk_nib)
+     implicit none
+     integer,intent(in)::n
+     real(8),intent(in)::th_eta,dt,mtk_nib
+     real(8),intent(inout)::v(3*N,3)
+     integer::i
+     do i=1,3*N
+         v(i,:)=v(i,:) - dt*(th_eta +(1+3/(6*N-3))* mtk_nib)*v(i,:)/2
+     end do
+     
+     
+     end subroutine
+     
+     
+     subroutine mtk_box(l,mtk_nib,dt)
+     implicit none
+     real(8),intent(in)::mtk_nib,dt
+     real(8),intent(inout)::l
+     
+     l=l * dexp(dt*mtk_nib)
+     
+     
+     
+     end subroutine
+     
+     
+     
+     subroutine shift_coms(x,dt,mtk_nib,N,delec,l)
+     real(8),intent(in)::dt,mtk_nib,delec(4*n,2),l
+     integer,intent(in)::N
+     real(8),intent(inout)::x(4*n,3)
+     integer::i
+     real(8)::mol(3,3),cntr_mass(3),d_i(3,3)
+     
+     do i=1,N
+         mol(1,:)=x(i,:)
+         mol(2,:)=x(i+n,:)
+         mol(3,:)=x(i+2*n,:)
+         mol(2,:)=mol(2,:)+l*int((mol(1,:)-mol(2,:))*2/l)
+         mol(3,:)=mol(3,:)+l*int((mol(1,:)-mol(3,:))*2/l)
+         cntr_mass=delec(i,1)*mol(1,:)+delec(i+n,1)*mol(2,:)+delec(i+n*2,1)*mol(3,:)
+         cntr_mass=cntr_mass/(delec(i,1)+delec(i+n,1)+delec(i+n*2,1))
+         d_i(1,:)=mol(1,:)-cntr_mass
+         d_i(2,:)=mol(2,:)-cntr_mass
+         d_i(3,:)=mol(3,:)-cntr_mass
+         cntr_mass=cntr_mass*(1+dt*mtk_nib)
+         x(i,:)=cntr_mass+ d_i(1,:)
+         x(i+n,:)=cntr_mass+ d_i(2,:)
+         x(i+2*n,:)=cntr_mass+ d_i(3,:)
+         x(i,:)=periodic_bound(x(i,:),l)
+            x(i+N,:)=periodic_bound(x(i+N,:),l)
+            x(i+2*N,:)=periodic_bound(x(i+2*N,:),l)
+     end do
+     end subroutine
+     
+    end module
+        
+        
+        
+        
